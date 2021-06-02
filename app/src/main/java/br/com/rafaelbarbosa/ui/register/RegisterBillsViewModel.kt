9b405fbe078b42446390ad5ae83ae7b8e0f5badb
Rafael Barbosa <rafael.costa.barbosa@g.globo>
2021-06-02 20:38:38 -0300

    iniciando projeto

---
+++ app/src/main/java/br/com/rafaelbarbosa/ui/register/RegisterBillsViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.rafaelbarbosa.ui.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterBillsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/br/com/rafaelbarbosa/ui/signup/SignUpViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.rafaelbarbosa.ui.signup
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
