d401170ff8ec5597c4fcf8fa5ef1d756643ea60a
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2021-06-03 15:46:34 -0300

    adicionando propaganda no app

--- app/src/main/java/br/com/rafaelbarbosa/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/rafaelbarbosa/ui/home/HomeFragment.kt
@@ -8,6 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import br.com.rafaelbarbosa.R
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
@@ -29,6 +33,18 @@ class HomeFragment : Fragment() {
         btnGoToBillsPage.setOnClickListener{
             findNavController().navigate(R.id.registerBillsFragment)
         }
+
+        //Inicialização do Google AdMob
+        MobileAds.initialize(requireContext())
+
+        //Widget que exibirá o anúncio
+        val adView = view.findViewById<AdView>(R.id.adView)
+
+        //Requisitar um anúncio
+        val adRequest = AdRequest.Builder().build()
+
+        //Lançar o anúncio no widget próprio
+        adView.loadAd(adRequest)
     }
 
 }
